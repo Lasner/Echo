9efe52fb1a3c2dd8de87ad7c7a4ac8ca790090df
Lasner <Lauri.Lasner@tptlive.ee>
2016-10-19 15:16:13 +0300

    Echo

--- src/Echo.java
+++ src/Echo.java
@@ -9,6 +9,7 @@ public class Echo {
         String line;
         Scanner in = new Scanner(System.in);
 
+
         System.out.print("Kirjuta midagi:");
         line = in.nextLine();
         System.out.println("Sa kirjutasid: " + line);
@@ -16,6 +17,35 @@ public class Echo {
         System.out.print("Kirjuta midagi muud: ");
         line = in.nextLine();
         System.out.println("Sa kirjutasid: " + line);
+
+        int inch;
+        double cm;
+        double cmPerInch = 2.54;
+        final double CM_PER_INCH = 2.54;
+
+        System.out.print("How many inches? ");
+        inch = in.nextInt();
+        cm = inch * cmPerInch;
+
+        System.out.print(inch + " in = ");
+        System.out.println(cm + " cm");
+
+        int esimenearv;
+        int teinearv;
+        int summa;
+
+        System.out.print("Kirjuta üks arv: ");
+        esimenearv = in.nextInt();
+        System.out.print("Kirjuta teine arv: ");
+        teinearv = in.nextInt();
+        summa = esimenearv + teinearv;
+        System.out.print("Arvude summa on: " + summa);
+
+
+
+
+
+
 
     }
 
